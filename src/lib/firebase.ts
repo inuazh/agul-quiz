efab75765285644cfeefe687afb364923c7cbcaf
inuazh <inuazh.kz@gmail.com>
2026-02-02 22:41:07 +0300

    firebase_config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,13 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, signInAnonymously } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY as string,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN as string,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID as string,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID as string,
-};
+import { firebaseConfig } from "./firebaseConfig";
 
 const app = initializeApp(firebaseConfig);
 
---
+++ src/lib/firebaseConfig.ts
@@ -0,0 +1,6 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA_3k7Syf7g3ej0ASW16Q7_m4PJZ6BByxg",
+  authDomain: "agul-quiz.firebaseapp.com",
+  projectId: "agul-quiz",
+  appId: "1:123456:web:abcd",
+};
